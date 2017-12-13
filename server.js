3acf89a6f226b99948dae957c766843215adbf04
Paul Bird <pbird@thinkingjuice.co.uk>
2017-12-13 23:44:13 +0000

    Can easily add times

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const serve = require('serve-static');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const shortId = require('shortid');
 const app = Express();
 
 //set up the db
@@ -18,9 +19,39 @@ db.defaults({
 //create a new router
 let router = Router();
 
-app.use('/', serve('./src/dist/'));
-app.use(router);
+router.post('/time', (req, res) => {
+    let { name, time } = req.body;
+    if (typeof name === undefined && typeof time === undefined) {
+        res.status(400).json({
+            message: 'Need to provide both a name and time'
+        });
+    } else {
+        if (time !== '' && name !== "") {
+            let id = shortId.generate();
+            db.get('times').push({
+                id,
+                time,
+                name
+            }).write();
+            res.status(200).json({
+                message: 'succesfully added time',
+                entry: {
+                    id,
+                    name,
+                    time
+                }
+            });
+        } else {
+            res.status(400).json({
+                message: "You need to provide actual values for name and time"
+            })
+        }
+    }
+});
+
 app.use(bodyParser.json());
+app.use('/', serve('./src/dist/'));
+app.use('/api', router);
 
 app.listen(7000, () => {
     console.log(`App started and listening on port 7000`);
