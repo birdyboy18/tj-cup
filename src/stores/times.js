b413a51d01be43de9b3294fd95a575821f45c9d8
Paul Bird <pbird@thinkingjuice.co.uk>
2017-12-14 01:23:48 +0000

    Can now add a time to the api, uses relative path ready for build

--- src/stores/times.js
+++ src/stores/times.js
@@ -1,6 +1,7 @@
 module.exports = store
 
 function store(state, emitter) {
+    const url = 'api/time';
     state.times = []
 
     state.newTime = {
@@ -15,14 +16,29 @@ function store(state, emitter) {
 
         emitter.on('time:add', () => {
             if (state.newTime.name !== '' && state.newTime.time !== '') {
-                state.times.push(state.newTime); //push new time into times list
-                state.newTime = { //reset it's state
-                    name: '',
-                    time: ''
-                }
-                state.modalOpen = false; //close the modal
-                emitter.emit(state.events.RENDER);
+                // state.times.push(state.newTime); //push new time into times list
+                // state.newTime = { //reset it's state
+                //     name: '',
+                //     time: ''
+                // }
+                addTime(state.newTime).then(json => {
+                    console.log(json);
+                    state.modalOpen = false; //close the modal
+                    emitter.emit('time:fetch');
+                    emitter.emit(state.events.RENDER);
+                }).catch(err => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
             }
+        })
+
+        emitter.on('time:fetch', () => {
+            fetchTimes().then(json => {
+                state.times = json;
+                emitter.emit(state.events.RENDER);
+            })
         })
 
         //lets fetch all of the results from the api
@@ -33,9 +49,24 @@ function store(state, emitter) {
     })
 
     function fetchTimes() {
-        let url = 'https://localhost:7000/api/time';
         return fetch(url).then(res => {
             return res.json();
+        })
+    }
+
+    function addTime(time) {
+        let body = {
+            time: time.time,
+            name: time.name
+        }
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return fetch(url, {
+            method: 'post',
+            headers: headers,
+            body: JSON.stringify(body)
+        }).then(res => {
+            return res.json(body);
         })
     }
 }
